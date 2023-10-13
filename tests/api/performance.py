ba54fac09d41286101c0ad6c8ce8ec51ec64bfcc
mhh <mike.hukiewitz@robotter.ai>
2023-10-13 19:46:27 +0200

    Add test to measure performance of preprocessing ugly CSV files.

--- tests/api/performance.py
+++ tests/api/performance.py
@@ -18,3 +18,20 @@ def test_preprocess_csv_performance(client, account, big_csv):
     assert end - start < 1
     assert response.status_code == 200
     assert response.num_bytes_downloaded >= big_csv.__sizeof__()
+
+
+def test_preprocess_ugly_csv(client, account, ugly_big_csv):
+    token = login_with_signature(client, account)
+
+    # measure time
+    start = time.time()
+    response = client.post(
+        "/timeseries/csv",
+        headers={"Authorization": f"Bearer {token}"},
+        files={"data_file": ("test.csv", ugly_big_csv, "text/csv")},
+    )
+    end = time.time()
+    print(f"Upload took {end - start} seconds")
+    assert end - start < 1
+    assert response.status_code == 200
+    assert response.num_bytes_downloaded >= ugly_big_csv.__sizeof__()
